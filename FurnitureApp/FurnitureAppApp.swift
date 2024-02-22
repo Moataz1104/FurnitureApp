7b96a7e6a54f371876f48e18f2d5fbcd47cdc517
Moataz1104 <moataz.mohamed.id@gmail.com>
2024-02-22 04:21:50 +0200

    Initial Commit

---
+++ FurnitureApp/FurnitureAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurnitureAppApp.swift
+//  FurnitureApp
+//
+//  Created by Moataz Mohamed on 22/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct FurnitureAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
